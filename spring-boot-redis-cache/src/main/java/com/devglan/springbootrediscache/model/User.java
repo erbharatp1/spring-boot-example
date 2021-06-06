7534c59d35a727180e8174bdd124db6dba68ac25
Bharat Patel <40136386+erbharatp1@users.noreply.github.com>
2021-06-06 20:22:27 +0530

    first commit

---
+++ spring-boot-redis-cache/src/main/java/com/devglan/springbootrediscache/model/User.java
@@ -0,0 +1,37 @@
+package com.devglan.springbootrediscache.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+@Entity
+public class User implements Serializable {
+@Id
+    private String id;
+    private String name;
+    private int age;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
